131ead5a3231c613a97224ef49a04abc75fb4a49
stivan <JohnSmitoff@gmail.com>
2019-04-14 12:46:34 +0300

    added sex choice field (M or F) to owner

--- animals/models.py
+++ animals/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 
 class Owner(models.Model):
+    OWNER_SEX = (("M", "Mail"), ("F", "Femail"))
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
+    owner_sex = models.CharField(max_length=1,choices=OWNER_SEX)
 
     def __str__(self):
         return f"owner_id -> {self.pk} : name - {self.first_name} {self.last_name} "
