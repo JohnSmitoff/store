c97a6ebceefb0a57d7912c8fb8bb4cb242c645df
stivan <JohnSmitoff@gmail.com>
2019-04-14 12:23:47 +0300

    Database table Owner added to db

--- animals/admin.py
+++ animals/admin.py
@@ -4,3 +4,4 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Animal)
+admin.site.register(models.Owner)
--- animals/models.py
+++ animals/models.py
@@ -3,6 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+class Owner(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"owner_id -> {self.pk} : name - {self.first_name} {self.last_name} "
+
+
 class Animal(models.Model):
     KIND_CHOICES = (("D", "Dog"), ("C", "Cat"))
 
@@ -12,6 +20,7 @@ class Animal(models.Model):
     description = models.TextField()
     image_url = models.URLField()
     kind = models.CharField(max_length=1, choices=KIND_CHOICES)
+    owner = models.ForeignKey(Owner, on_delete=None, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.name} and I am a {self.kind} - {self.age} years old"
+        return f"{self.name} and I am a {self.kind} - {self.age} years old with owner - ({self.owner})"
